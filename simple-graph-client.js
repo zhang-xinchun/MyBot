1bd12e8044c82378ce349af01ce7199e655f28c9
zhang.xc <zhang_xinchun@pactera.com>
2020-02-24 14:46:12 +0800

    add Schedule api

--- simple-graph-client.js
+++ simple-graph-client.js
@@ -110,11 +110,11 @@ class SimpleGraphClient {
             .post({
                 schedules: ['zhang_xinchun@pactera.com'],
                 startTime: {
-                    dateTime: '2019-06-15T09:00:00',
+                    dateTime: '2019-10-15T09:00:00',
                     timeZone: 'Pacific Standard Time'
                 },
                 endTime: {
-                    dateTime: '2020-12-15T18:00:00',
+                    dateTime: '2019-12-15T18:00:00',
                     timeZone: 'Pacific Standard Time'
                 },
                 availabilityViewInterval: 60
