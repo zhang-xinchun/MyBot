19b90006035ba77fd766c19707d0de477a342403
zhang.xc <zhang_xinchun@pactera.com>
2020-02-24 16:50:46 +0800

    fixed start file

--- oAuthHelpers.js
+++ oAuthHelpers.js
@@ -45,7 +45,7 @@ class OAuthHelpers {
         }
 
         const client = new SimpleGraphClient(tokenResponse.token);
-        var schedule = await client.getSchedule() || '';
+        var schedule = await client.getSchedule() || 'None';
         await context.sendActivity(`Schedule information ${ schedule }`);
     }
 
